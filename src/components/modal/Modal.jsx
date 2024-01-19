2159ac3c01fdc013d5377f261cab4078bfc5af30
CCargill1 <116306171+CCargill1@users.noreply.github.com>
2024-01-19 15:28:27 -0500

    Add files via upload

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,27 +1,64 @@
-import React from 'react'
-import "./Modal.css"
+import React from "react";
+import "./Modal.css";
 
-import TextField from '@mui/material/TextField/TextField'
-import Button from '@mui/material/Button/Button'
-import logo from "../../assets/Teldio-Logo-Color.png"
+import logo from "../../assets/Teldio-Logo-Color.png";
 
-import Uploader from '../uploader/Uploader'
+import Uploader from "../uploader/Uploader";
+import { Typography } from "@mui/material";
 const Modal = () => {
   return (
     <>
-    <div className='container'>
-        <div className='modal'>
-            <div className='bannerContainer'>
-                <img src={logo} alt="Teldio Logo" className='logo' />
-            </div>
-            
-            <Uploader />            
-            
+      <div
+        style={{
+          width: "100%",
+          height: "70px",
+          backgroundColor: "#424242",
+          display: "flex",
+          justifyContent: "right",
+          alignItems: "center",
+          fontFamily: "fonts/Oxygen-Light",
+        }}
+      >
+        <Typography
+          sx={{
+            width: "100%",
+            color: "white",
+            fontFamily: "inherit",
+            textAlign: "center",
+          }}
+          variant="h4"
+        >
+          Teldio File Uploader
+        </Typography>
+        <img
+          src="assets/logo.png"
+          alt="teldio logo"
+          style={{
+            height: "35px",
+            marginRight: "15px",
+            position: "absolute",
+          }}
+        />
+      </div>
+      <div className="container">
+        <div
+          style={{
+            backgroundColor: "white",
+            marginTop: "50px",
+            width: "clamp(400px, 60%, 500px)",
+            borderRadius: "30px",
+            boxShadow: "15px 15px 19px rgba(0, 0, 0, 0.1)",
+            padding: "20px 20px 40px 20px",
+          }}
+        >
+          <div className="bannerContainer">
+            <img src={logo} alt="Teldio Logo" className="logo" />
+          </div>
+          <Uploader />
         </div>
-        
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
--- src/components/uploader/Uploader.jsx
+++ src/components/uploader/Uploader.jsx
@@ -1,12 +1,76 @@
 import React from "react";
 import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
-import CircularProgress from "@mui/material/CircularProgress";
-import Box from "@mui/material/Box";
+import { ManagedUpload } from "aws-sdk/clients/s3";
 
 import Button from "@mui/material/Button/Button";
 import "./Uploader.css";
 import TextField from "@mui/material/TextField/TextField";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+import { Alert, Box, Snackbar, Stack, Typography } from "@mui/material";
+import styled from "@emotion/styled";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import { red } from "@mui/material/colors";
+
+const RoundedTextField = styled(TextField)({
+  marginTop: "20px",
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderRadius: "30px",
+    },
+  },
+});
+
+const BigButton = styled(Button)(({ theme }) => ({
+  width: "90%",
+  height: "40px",
+  margin: "20px",
+  borderRadius: "50px",
+  backgroundColor: "#A0AD39",
+  "&:hover": {
+    backgroundColor: "#c1cd66",
+  },
+  fontSize: "17px",
+  color: "white",
+}));
+
+const CancelButton = styled(Button)(({ theme }) => ({
+  color: "#686868",
+  fontWeight: "bold",
+  fontFamily: "fonts/Oxygen-Light, Segoe UI, Roboto",
+  textTransform: "none",
+  fontSize: "15px",
+  marginTop: "20px",
+}));
+
+function LinearProgressWithLabel(props) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+const BorderLinearProgress = styled(LinearProgressWithLabel)(() => ({
+  height: 10,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: "#e7ebca",
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: "#A0AD39",
+  },
+}));
 
 const Uploader = () => {
   const [file, setFile] = useState();
@@ -14,10 +78,23 @@ const Uploader = () => {
   const [message, setMessage] = useState();
   const [fileDesc, setFileDesc] = useState("");
   const [emailMessage, setEmailMessage] = useState();
-  const [uploading, setUploading] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState();
 
   const AWS = require("aws-sdk");
+  const [open, setOpen] = React.useState(false);
 
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    setMessage("")
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
   function cancelProcess() {
     setMessage("");
     setFile(null);
@@ -33,28 +110,10 @@ const Uploader = () => {
 
   const handleChange = (file) => {
     setMessage("");
+    setUploadProgress(null);
     setEmailMessage("");
     setFile(file);
   };
-
-  function sendEmail(userEmail, filename, fileLocation) {
-    const data = {
-      fileName: filename,
-      emailAddress: userEmail,
-      location: fileLocation,
-    };
-
-    let url = process.env.REACT_APP_HYYPHENAPIURL;
-
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    setEmail("");
-  }
 
   function sendEmailViaEmailJS(userEmail, filename, fileLocation) {
     var data = {
@@ -83,6 +142,8 @@ const Uploader = () => {
   function openFileExplorer() {
     const input = document.createElement("input");
     input.type = "file";
+    setUploadProgress(null);
+
     input.onchange = (e) => {
       const file = e.target.files[0];
       setMessage("");
@@ -94,8 +155,10 @@ const Uploader = () => {
 
   function uploadFileToS3() {
     if (email.length > 0) {
-      setUploading(true);
-      const s3 = new AWS.S3();
+      const s3 = new AWS.S3({
+        accessKeyId: process.env.REACT_APP_AWSACCESSKEY,
+        secretAccessKey: process.env.REACT_APP_AWSSECRETKEY,
+      });
 
       const fileName = file.name;
       const fileType = file.type;
@@ -108,22 +171,27 @@ const Uploader = () => {
         ACL: "public-read",
       };
 
-      s3.config.update({
-        accessKeyId: process.env.REACT_APP_AWSACCESSKEY,
-        secretAccessKey: process.env.REACT_APP_AWSSECRETKEY,
+      const upload = new ManagedUpload({
+        params,
+        service: s3,
       });
 
-      s3.upload(params, function (err, data) {
-        setUploading(false);
+      upload.on("httpUploadProgress", function (progress) {
+        const percentage = Math.round((progress.loaded / progress.total) * 100);
+        setUploadProgress(percentage);
+      });
+
+      upload.send(function (err, data) {
         if (err) {
           console.log(err);
           setMessage("Upload Failed, Please try Again");
+          setUploadProgress(0);
         } else {
           console.log(data);
           setMessage("Uploaded Successfully");
-          //sendEmail(email, fileName, data.Location)
           sendEmailViaEmailJS(email, fileName, data.Location);
           setFile(null);
+          setUploadProgress(100);
         }
       });
     } else {
@@ -131,73 +199,162 @@ const Uploader = () => {
     }
   }
 
+  // function uploadFileToS3() {
+  //   if (email.length > 0) {
+  //     const s3 = new AWS.S3();
+
+  //     const fileName = file.name;
+  //     const fileType = file.type;
+
+  //     const params = {
+  //       Bucket: "tools.teldio.com/files/tbox",
+  //       Key: fileName,
+  //       Body: file,
+  //       ContentType: fileType,
+  //       ACL: "public-read",
+  //     };
+
+  //     s3.config.update({
+  //       accessKeyId: process.env.REACT_APP_AWSACCESSKEY,
+  //       secretAccessKey: process.env.REACT_APP_AWSSECRETKEY,
+  //     });
+
+  //     s3.upload(params, function (err, data) {
+  //       if (err) {
+  //         console.log(err);
+  //         setMessage("Upload Failed, Please try Again");
+  //       } else {
+  //         console.log(data);
+  //         setMessage("Uploaded Successfully");
+  //         //sendEmail(email, fileName, data.Location)
+  //         sendEmailViaEmailJS(email, fileName, data.Location);
+  //         setFile(null);
+  //       }
+  //     });
+  //   } else {
+  //     setEmailMessage("Please Enter A Valid Email");
+  //   }
+  // }
+
   return (
     <>
       <div className="itemContainer">
         {!file ? (
-          <FileUploader handleChange={handleChange} name="file" />
+          <div style={{ marginTop: "80px", marginBottom: "60px" }}>
+            <FileUploader handleChange={handleChange} name="file" />
+          </div>
         ) : null}
-        {!file ? (
-          <Button variant="contained" fullWidth onClick={openFileExplorer}>
+        {!file && (
+          <BigButton variant="contained" fullWidth onClick={openFileExplorer}>
             Add File
-          </Button>
-        ) : (
-          <Button
-            variant="contained"
-            fullWidth
-            onClick={openFileExplorer}
-            disabled
-          >
-            Add File
-          </Button>
+          </BigButton>
         )}
-        {file ? <h2 className="textField">{file.name}</h2> : null}
-        {file && !uploading ? (
-          <TextField
-            id="outlined-basic"
-            label="Email"
-            variant="outlined"
-            onChange={(e) => setEmailWithChecks(e.target.value)}
-          />
-        ) : null}
-        {file && !uploading ? (
-          <TextField
-            id="outlined-basic"
-            label="Description"
-            variant="outlined"
-            onChange={(e) => setFileDesc(e.target.value)}
-          />
-        ) : null}
-        {file ? <h4 className="emailMessage">{emailMessage}</h4> : null}
-        {uploading ? (
-          <Box
-            sx={{ display: "flex", width: "100%", justifyContent: "center" }}
-          >
-            <CircularProgress />
-          </Box>
-        ) : null}
-        {file && !uploading ? (
-          <Button
+        {file && (
+          <div>
+            <Typography
+              variant="subtitle1"
+              sx={{
+                textAlign: "center",
+                maxWidth: "100%",
+                wordWrap: "break-word",
+                marginTop: "40px",
+              }}
+            >
+              File to Upload:
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              sx={{
+                textAlign: "center",
+                maxWidth: "100%",
+                wordWrap: "break-word",
+                marginTop: "5px",
+              }}
+            >
+              {file.name}
+            </Typography>
+          </div>
+        )}
+        {file && (
+          <div>
+            <RoundedTextField
+              id="outlined-basic"
+              label="Your Email Address*"
+              error={emailMessage}
+              fullWidth
+              variant="outlined"
+              onChange={(e) => setEmailWithChecks(e.target.value)}
+            />
+            {emailMessage && (
+              <Stack
+                direction="row"
+                gap={1}
+                alignItems="center"
+                justifyContent="left"
+                sx={{
+                  marginTop: "5px",
+                  color: "#D32E2E",
+                  fontFamily: "fonts/Oxygen-Light",
+                }}
+              >
+                <ErrorOutlineIcon />
+                <Typography
+                  variant="subtitle2"
+                  sx={{ fontFamily: "fonts/Oxygen-Light" }}
+                >
+                  {emailMessage}
+                </Typography>
+              </Stack>
+            )}
+            <RoundedTextField
+              id="outlined-basic"
+              label="Description of File"
+              variant="outlined"
+              fullWidth
+              onChange={(e) => setFileDesc(e.target.value)}
+            />
+          </div>
+        )}
+        {file && uploadProgress && (
+          <div style={{ marginTop: "20px" }}>
+            <BorderLinearProgress
+              variant="determinate"
+              value={uploadProgress}
+            />
+          </div>
+        )}
+        {file ? (
+          <BigButton
             variant="contained"
             className="button"
             fullWidth
             onClick={uploadFileToS3}
+            disabled={uploadProgress}
           >
             Upload To Teldio
-          </Button>
+          </BigButton>
         ) : null}
-        {message ? <h1>{message}</h1> : null}
+        <Snackbar open={message} autoHideDuration={6000} onClose={handleClose} sx={{marginTop: "50px"}} anchorOrigin={{ vertical: "top", horizontal: "right" }}>
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            {message}
+          </Alert>
+        </Snackbar>
       </div>
 
-      {file && !uploading ? (
-        <Button
+      {file ? (
+        <CancelButton
           variant="text"
           className="cancelButton"
           fullWidth
           onClick={cancelProcess}
         >
           Cancel
-        </Button>
+        </CancelButton>
       ) : null}
     </>
   );
